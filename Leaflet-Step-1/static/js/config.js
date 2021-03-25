b54bc45be38ef7acb41bcd84f7f3a731acfa6cc2
Matthew Padgett <matthewpadgett@matthews-mbp-2.myfiosgateway.com>
2021-03-25 12:11:00 -0400

    Pushing up code

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibXBhZGdldHQwMDQiLCJhIjoiY2ttNW1yam93MGZucDJucHJmc2RnNjIwYiJ9.5niXhXNQ9enYmnLfFeWVjA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,99 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+var earthquakes = new L.LayerGroup();
+
+var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var baseMaps = satellitemap;
+var overlayMaps = earthquakes;
+
+var myMap = L.map("mapid", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [satellitemap, earthquakes]
+});
+
+// Using D3 to pull in data from URL
+d3.json(queryUrl, function(data) {
+  function markerSize(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 3;
+  }
+  //Creating circles as markers
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: chooseColor(feature.properties.mag),
+      color: "#000000",
+      radius: markerSize(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // Magnitude levels
+  function chooseColor(magnitude) {
+    switch (true) {
+    case magnitude > 5:
+        return "#581845";
+    case magnitude > 4:
+        return "#900C3F";
+    case magnitude > 3:
+        return "#C70039";
+    case magnitude > 2:
+        return "#FF5733";
+    case magnitude > 1:
+        return "#FFC300";
+    default:
+        return "#DAF7A6";
+    }
+  }
+
+  L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h4>Location: " + feature.properties.place + 
+        "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+        "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+    }
+  })
+  .addTo(earthquakes);
+  earthquakes.addTo(myMap);
+
+  
+  // Legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"), 
+    magnitudeLevels = [0, 1, 2, 3, 4, 5];
+
+    div.innerHTML += "<h3>Magnitude</h3>"
+
+    for (var i = 0; i < magnitudeLevels.length; i++) {
+        div.innerHTML +=
+            '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
+            magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+
+  legend.addTo(myMap);
+
+
+});
+
+
+
